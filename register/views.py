2849581980b02cd815b6639238fd89dce2b17f52
xiao ming <minhnguyen47431@gmail.com>
2026-05-13 12:19:46 +0700

    fix

--- register/views.py
+++ register/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     return render(request, 'register/register.html')
+
+
+def upload(request):
+    return render(request, 'register/register-upload.html')
